8d172b0ac7956bc2301ed4815c64e7c35cc6daed
Carlos Martínez <carlos.martinezg@estudiants.urv.cat>
2022-12-02 21:59:54 +0100

    Javadoc added

---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"ActorModel.Data"},{"l":"ActorModel.Data.Exceptions"},{"l":"ActorModel.Data.Messages"},{"l":"ActorModel.Data.Messages.Insult"},{"l":"ActorModel.Data.Tests"},{"l":"All Packages","u":"allpackages-index.html"}];updateSearchResults();
